3297ab76b5c354564395693e36cec242941d1fea
mehediFluttere <mehedii.edu.bd@gmail.com>
2024-09-04 22:11:37 +0600

    counter provider created

---
+++ lib/provider/counter_provider.dart
@@ -0,0 +1,24 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+//import 'package:riverpod_state_management/provider/counter_provider.g.dart'.
+part 'counter_provider.g.dart';
+
+
+
+@riverpod 
+
+class Counter extends _$Counter{
+  
+  @override
+
+ int build() => 0;
+
+ void increment () => state++;
+  
+}
+@riverpod
+
+
+ List myList (ref){
+  return ['a','b','c'];
+
+ }
